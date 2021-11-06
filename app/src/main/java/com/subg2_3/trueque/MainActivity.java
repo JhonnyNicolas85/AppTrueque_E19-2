8442e9ea144e71c650473697f3da26402ef12fd7
Ilmer Ordoñez <76207799+ilmerozu@users.noreply.github.com>
2021-11-05 20:20:59 -0500

    Add files via upload
    Se crea esqueleto de  aplicacion

---
+++ app/src/main/java/com/subg2_3/trueque/MainActivity.java
@@ -0,0 +1,14 @@
+package com.subg2_3.trueque;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
