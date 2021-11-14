035459b3c6e7c041361c37d66400d64929f4f762
Ilmer Ordoñez <ilmeroqoutlook.com>
2021-11-14 03:15:48 -0500

    Agrego splash

---
+++ app/src/main/java/com/subg2_3/trueque/StartActivity.java
@@ -0,0 +1,30 @@
+package com.subg2_3.trueque;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        TimerTask tarea = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(StartActivity.this,MainActivity.class );
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer timeDelay =new Timer();
+        timeDelay.schedule(tarea, 5000);
+
+    }
+}
