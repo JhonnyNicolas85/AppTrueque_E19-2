96c9808c26f54c6b18ac641f5c44bffbfc3e1f45
Ilmer Ordoñez <ilmeroqoutlook.com>
2021-11-14 02:01:28 -0500

    Se crea esqueleto de pantalla donde se despliegan los productos

--- app/src/main/java/com/subg2_3/trueque/MainActivity.java
+++ app/src/main/java/com/subg2_3/trueque/MainActivity.java
@@ -2,7 +2,9 @@ package com.subg2_3.trueque;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void goToAddProduct (View view) {
+        Intent intentAdd = new Intent(this, AddActivity.class);
+        startActivity(intentAdd);
     }
 }
