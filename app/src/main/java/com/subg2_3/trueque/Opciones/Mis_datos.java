791ffdce69513d412b6da9cb82120a6aa6bc70cd
JosephToq <steventoq@hotmail.com>
2021-12-04 18:51:16 -0500

    Creo el loyout pare el perfil del ususario agrego unas imagenes

---
+++ app/src/main/java/com/subg2_3/trueque/Opciones/Mis_datos.java
@@ -0,0 +1,16 @@
+package com.subg2_3.trueque.Opciones;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.subg2_3.trueque.R;
+
+public class Mis_datos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mis_datos);
+    }
+}
